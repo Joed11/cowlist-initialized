d3335cc7b9ef4fe403d1b9a60b6dc9debff3bae4
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-13 16:48:06 -0400

    Set up main app file
    import sub-components
    import jquery
    import api methods
    Set initial state
    add method to add cows to database
    add method to refresh the list of cows
    add method to change the featured cow
    populate render with sub components and provide their props

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CowList from './CowList'
+import CowSpotlight from './CowSpotlight'
+import SubmitCow from './SubmitCow'
+import exampleCowList from '../../server/db/exampleCowData.js'
+import addACow from '../lib/addCows.js'
+import getAllCows from '../lib/getCows.js'
+import $ from "jquery";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cowList: [],
+      currentCow: null
+    }
+  }
+
+  componentDidMount() {
+    this.refreshCows();
+  }
+
+  addCow(newCow) {
+    addACow(newCow, (response) => {
+      console.log('You maybe posted a cow', response)
+      this.refreshCows();
+    })
+  }
+
+  refreshCows() {
+    getAllCows((result) => {
+      console.log(result);
+      this.setState({
+        cowList: result
+      });
+    })
+  }
+
+  changeCurrentCow(targetCow) {
+    console.log('changing the cow')
+    this.setState({
+      currentCow: targetCow
+    })
+  }
+
+  deleteCow() {
+
+  }
+
   render() {
     return (
-    <div>Hello World!</div>
+      <div>
+        <h2>Cow List Exercise</h2>
+        <SubmitCow addCow={this.addCow.bind(this)}/>
+        <CowSpotlight cow={this.state.currentCow}/>
+        <CowList cows={this.state.cowList} changeCow={this.changeCurrentCow.bind(this)}/>
+      </div>
     );
   }
 }
