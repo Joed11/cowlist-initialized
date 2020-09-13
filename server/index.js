8fb5de3a257ea20a00be35f5a938f130085c3c05
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-13 16:54:16 -0400

    Add morgan middleware and change port

--- server/index.js
+++ server/index.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 const models = require('./models')
 const bodyParse = require('body-parser')
+const morgan = require('morgan')
 
 app.use(express.static('./client/dist'))
 app.use(bodyParse.json())
+app.use(morgan(':method :url :status :res[content-length] :req[header] - :response-time ms'))
+
+
+app.set('views', `../client/dist`);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('Hello World')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+//Create One All
+app.post('/api/cows', (req, res) => {
+  console.log(req.body)
+  return models.cows.create(req, res)
+    .catch((error) => {
+      console.log('ERROR')
+    })
+})
+
+//Read All
 app.get('/api/cows', (req, res) => {
   return models.cows.getAll(req, res)
     .catch((error) => {
@@ -20,9 +35,3 @@ app.get('/api/cows', (req, res) => {
     })
 })
 
-app.post('/api/cows', (req, res) => {
-  return models.cows.create(req, res)
-    .catch((error) => {
-      console.log('ERROR')
-    })
-})
