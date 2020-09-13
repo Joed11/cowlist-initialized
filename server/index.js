c809542c1b448a220e0d5aaba016949258d30afd
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-13 12:11:43 -0400

    Intital setup and routing
    Add body parsing middleware
    Load models module
    Route get requests to /api/cows
    Route post reequests to /api/cows

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const models = require('./models')
+const bodyParse = require('body-parser')
 
 app.use(express.static('./client/dist'))
+app.use(bodyParse.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/api/cows', (req, res) => {
+  return models.cows.getAll(req, res)
+    .catch((error) => {
+      console.log('ERROR')
+    })
+})
+
+app.post('/api/cows', (req, res) => {
+  return models.cows.create(req, res)
+    .catch((error) => {
+      console.log('ERROR')
+    })
+})
